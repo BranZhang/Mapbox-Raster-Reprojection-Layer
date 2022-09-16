6a6c95ae3a5dc6cbd3b98054aa63dceb540afe26
BranZhang <zcqzcq@outlook.com>
2022-09-16 11:33:27 +0800

    init.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    // optimization: {
+    //     runtimeChunk: 'single',
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: 'public/index.html'
+        }),
+    ],
+};
